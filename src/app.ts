229f6b29e442eb2fd360f8df4fe412dfff8e58a8
davidroardev <david_roa_06@hotmail.com>
2024-09-13 16:34:08 -0500

    Añadido Middleware error

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express, { response, Router,} from "express";
 import { userRoutes } from "./routes/user_routes";
 import { autosRoutes } from "./routes/autos_routes";
+import { errorHandler } from "./middleware/error";
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.EXPRESS_PORT;
 
-app.use(express.json())
+app.use(express.json());
+app.use(errorHandler);
 
 app.use(autosRoutes);
 app.use (userRoutes);
