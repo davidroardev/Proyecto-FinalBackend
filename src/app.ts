18f37e6e19040f8819d0dbe136d990108fff1a31
davidroardev <david_roa_06@hotmail.com>
2024-09-13 18:52:08 -0500

    Se modifico la funcion generateToken para que use la informaciion de la database

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,20 @@ import express, { response, Router,} from "express";
 import { userRoutes } from "./routes/user_routes";
 import { autosRoutes } from "./routes/autos_routes";
 import { errorHandler } from "./middleware/error";
+import cors from "cors";
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.EXPRESS_PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
 
 app.use(autosRoutes);
 app.use (userRoutes);
+
 app.listen(port, () => {
     console.log(`Proyecto final app listening on port ${port}`)
   })
--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,12 +1,18 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, response, Response } from "express"
 import jwt from "jsonwebtoken"
+import pool from "../database/db_coneccion";
 require('dotenv').config();
 
-export const generateToken = (req:Request,  res:Response): Response =>{
-    const userName = req.body.username; 
+export const generateToken = async (req: Request, response:Response): Promise<Response> => {
+    const userName = req.body.userName;
     const password = req.body.password;
-    const user = {name: userName, password:password};
-    const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}`, {expiresIn:'1h'});
-    return res.status(200).json({accessToken});
+    const query = await pool.query('SELECT * FROM users WHERE user_name = $1 AND password = $2', [userName, password]);
+    const user = query.rows[0];
+    if (query.rowCount !== null  && query.rowCount > 0){
+        const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}`, {expiresIn: '1h'});
+        return response.status(200).json({accessToken});
+    } else {
+        return response.status(400).json('User Not found');
+    }
 };
 
